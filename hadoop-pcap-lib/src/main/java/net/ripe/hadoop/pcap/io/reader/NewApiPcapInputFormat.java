f77a5e88781dfd0e83010e5ef5241348ea9c605b
Vince Gonzalez <vince@ip-192-168-100-120.ec2.internal>
2015-12-11 15:09:21 -0500

    added InputFormat and RecordReader using new MapReduce API - enables use with Spark Streaming

---
+++ hadoop-pcap-lib/src/main/java/net/ripe/hadoop/pcap/io/reader/NewApiPcapInputFormat.java
@@ -0,0 +1,65 @@
+package net.ripe.hadoop.pcap.io.reader;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+
+import net.ripe.hadoop.pcap.PcapReader;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.ObjectWritable;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.CompressionCodecFactory;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+
+public class NewApiPcapInputFormat extends org.apache.hadoop.mapreduce.lib.input.FileInputFormat<LongWritable, ObjectWritable> {
+    static final String READER_CLASS_PROPERTY = "net.ripe.hadoop.pcap.io.reader.class";
+
+    public static final Log LOG = LogFactory.getLog(NewApiPcapInputFormat.class);
+
+    public org.apache.hadoop.mapreduce.RecordReader<LongWritable, ObjectWritable> createRecordReader(org.apache.hadoop.mapreduce.InputSplit split, TaskAttemptContext context) throws IOException, InterruptedException {
+        FileSplit fileSplit = (FileSplit)split;
+        Path path = fileSplit.getPath();
+        LOG.info("Reading PCAP: " + path.toString());
+        long start = 0L;
+        long length = fileSplit.getLength();
+        return initPcapRecordReader(path, start, length, context);
+    }
+
+    public static NewApiPcapRecordReader initPcapRecordReader(Path path, long start, long length, TaskAttemptContext context) throws IOException {
+        Configuration conf = context.getConfiguration();
+        FileSystem fs = path.getFileSystem(conf);
+        FSDataInputStream baseStream = fs.open(path);
+        DataInputStream stream = baseStream;
+        CompressionCodecFactory compressionCodecs = new CompressionCodecFactory(conf);
+        final CompressionCodec codec = compressionCodecs.getCodec(path);
+        if (codec != null)
+            stream = new DataInputStream(codec.createInputStream(stream));
+
+        PcapReader reader = initPcapReader(stream, conf);
+        return new NewApiPcapRecordReader(reader, start, length, baseStream, stream);
+    }
+
+    public static PcapReader initPcapReader(DataInputStream stream, Configuration conf) {
+        try {
+            Class<? extends PcapReader> pcapReaderClass = conf.getClass(READER_CLASS_PROPERTY, PcapReader.class, PcapReader.class);
+            Constructor<? extends PcapReader> pcapReaderConstructor = pcapReaderClass.getConstructor(DataInputStream.class);
+            return pcapReaderConstructor.newInstance(stream);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    protected boolean isSplitable(JobContext context, Path filename) {
+        return false;
+    }
+}
---
+++ hadoop-pcap-lib/src/main/java/net/ripe/hadoop/pcap/io/reader/NewApiPcapRecordReader.java
@@ -0,0 +1,86 @@
+package net.ripe.hadoop.pcap.io.reader;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import net.ripe.hadoop.pcap.PcapReader;
+import net.ripe.hadoop.pcap.packet.Packet;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Seekable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.ObjectWritable;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+
+public class NewApiPcapRecordReader extends RecordReader<LongWritable, ObjectWritable> {
+    PcapReader pcapReader;
+    Iterator<Packet> pcapReaderIterator;
+    Seekable baseStream;
+    DataInputStream stream;
+
+    private LongWritable key = new LongWritable();
+    private ObjectWritable value = new ObjectWritable();
+    private FileSplit fileSplit;
+    private Configuration conf;
+
+    long packetCount = 0;
+    long start, end;
+
+    public NewApiPcapRecordReader(PcapReader pcapReader, long start, long end, Seekable baseStream, DataInputStream stream) throws IOException {
+        this.pcapReader = pcapReader;
+        this.baseStream = baseStream;
+        this.stream = stream;
+        this.start = start;
+        this.end = end;
+
+        pcapReaderIterator = pcapReader.iterator();
+    }
+
+    @Override
+    public void initialize(InputSplit inputSplit, TaskAttemptContext taskAttemptContext)
+            throws IOException, InterruptedException {
+        this.fileSplit = (FileSplit) inputSplit;
+        this.conf = taskAttemptContext.getConfiguration();
+    }
+
+    @Override
+    public void close() throws IOException {
+        stream.close();
+    }
+
+    @Override
+    public boolean nextKeyValue() throws IOException {
+        if (!pcapReaderIterator.hasNext())
+            return false;
+
+        this.key.set(++packetCount);
+        value.set(pcapReaderIterator.next());
+
+        return true;
+    }
+
+    @Override
+    public ObjectWritable getCurrentValue() {
+        return (ObjectWritable)value;
+    }
+
+    @Override
+    public LongWritable getCurrentKey() {
+        return key;
+    }
+
+    public long getPos() throws IOException {
+        return baseStream.getPos();
+    }
+
+    @Override
+    public float getProgress() throws IOException {
+        if (start == end)
+            return 0;
+        return Math.min(1.0f, (getPos() - start) / (float)(end - start));
+    }
+}
